cf49e987a4de9dfa7b44c4abc9395ac08f956367
mihir170901 <mihirchheda17@gmail.com>
2022-07-14 20:53:00 +0530

    as before

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = process.env.DATABASE   
+const mongoURI = "mongodb+srv://mihir17:Mihir1909@festivity.5frgj14.mongodb.net/test"  
 // const mongoURI = "mongodb://localhost:27017/festivity?readPreference=primary&appname=MongoDB%20Compass&ssl=false"    
 
 const connectToMongo = ()=>{
