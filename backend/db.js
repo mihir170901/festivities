eab0817054ea85a259f01201bec8f91804c7c5ab
mihir170901 <mihirchheda17@gmail.com>
2022-07-14 21:14:08 +0530

    deploying

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = process.env.MONGODB_URI   
+const mongoURI = process.env.MONGODB_URI || "mongodb://localhost:27017/festivity?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
 // const mongoURI = "mongodb://localhost:27017/festivity?readPreference=primary&appname=MongoDB%20Compass&ssl=false"    
 
 const connectToMongo = ()=>{
